e8badea71e9232a78611059cfca93dbb14401cdd
MatheusConfortini <matheusconfortini01@gmail.com>
2025-01-17 15:57:43 -0300

    testando

--- stage_1.sh
+++ stage_1.sh
@@ -7,51 +7,101 @@ source /home/pascal/LAMW/lamw4linux/etc/environment
 #Variaveis
 export ACBR_HOME=/var/jenkins_home/workspace/Teste-Pipeline/trunk2
 export ANDROID_HOME=/home/pascal/LAMW/sdk
-export DIR_LPI=$ACBR_HOME/Projetos/ACBrLib/Fontes/NFe
+
+
+ACBRLIB_LPI_DIR="$ACBR_HOME/Projetos/ACBrLib/Fontes/NFe"
+ACBRLIB_ANDROID_PROJECT_DIR="$ACBR_HOME/Projetos/ACBrLib/Android/NFe/ACBrLibNFe"
+JNILIBS_OUTDIR="$ACBRLIB_ANDROID_PROJECT_DIR/src/main/jniLibs"
+
 
 # Compila LIB com LAMW
-if [ -f "$DIR_LPI/ACBrLibNFeConsoleMT.lpi" ]; then
+if [ -f "$ACBRLIB_LPI_DIR/ACBrLibNFeConsoleMT.lpi" ]; then
   echo -e "Compilando LIB Android...\n"
-  
-  ${lazbuild_lamw} --bm=android-armeabi-v7a $DIR_LPI/ACBrLibNFeConsoleMT.lpi
+
+  ${lazbuild_lamw} --bm=android-armeabi-v7a $ACBRLIB_LPI_DIR/ACBrLibNFeConsoleMT.lpi
   if [ $? -ne 0 ]; then
     echo -e "Erro ao compilar para android-armeabi-v7a.\n"
     exit 1
   fi
 
-  ${lazbuild_lamw} --bm=android-arm64-v8a $DIR_LPI/ACBrLibNFeConsoleMT.lpi
+  ${lazbuild_lamw} --bm=android-arm64-v8a $ACBRLIB_LPI_DIR/ACBrLibNFeConsoleMT.lpi
   if [ $? -ne 0 ]; then
     echo -e "Erro ao compilar para android-arm64-v8a.\n"
     exit 1
   fi
   
   echo -e "Compilação concluída com sucesso.\n"
-  cp  $DIR_LPI/bin/Android/jniLibs $ACBR_HOME/Projetos/ACBrLib/Android/NFe/ACBrLibNFe -r
+  
+  echo -e "Copiando Libs para projeto Android. \n"
+  cp  $ACBRLIB_LPI_DIR/bin/Android/jniLibs -r $(dirname "$JNILIBS_OUTDIR")
+
 else
   echo -e "ACBrLibNFeConsoleMT.lpi não encontrado, verifique os arquivos.\n"
   exit 1
 fi
 
 
-# Compila LIB DEMO com LAMW
-if [ -f "$DIR_LPI/ACBrLibNFeConsoleMTDemo.lpi" ]; then
-  echo -e "Compilando LIB Android...\n"
-  
-  ${lazbuild_lamw} --bm=android-armeabi-v7a $DIR_LPI/ACBrLibNFeConsoleMTDemo.lpi
-  if [ $? -ne 0 ]; then
-    echo -e "Erro ao compilar para android-armeabi-v7a.\n"
-    exit 1
-  fi
+# copiar as bibliotecas do openssl para o projeto android
+cp $ACBR_HOME/DLLs/Android/OpenSSL/openssl-1.1.1d/arm-linux-androideabi/Dynamic/*  $JNILIBS_OUTDIR/armeabi-v7a
+cp $ACBR_HOME/DLLs/Android/OpenSSL/openssl-1.1.1d/aarch64-linux-android/Dynamic/*  $JNILIBS_OUTDIR/arm64-v8a
 
-  ${lazbuild_lamw} --bm=android-arm64-v8a $DIR_LPI/ACBrLibNFeConsoleMTDemo.lpi
-  if [ $? -ne 0 ]; then
-    echo -e "Erro ao compilar para android-arm64-v8a.\n"
-    exit 1
-  fi
-  
-  echo -e "Compilação concluída com sucesso.\n"
-  cp  $DIR_LPI/bin/Android/jniLibs $ACBR_HOME/Projetos/ACBrLib/Android/NFe/ACBrLibNFe -r
-else
-  echo -e "ACBrLibNFeConsoleMTDemo.lpi não encontrado, verifique os arquivos.\n"
-  exit 1
-fi
+#copiar a libxml2 para android 
+cp $ACBR_HOME/DLLs/Android/LibXML2/libxml2-2.9.10/arm-linux-androideabi/Dynamic/*.so $JNILIBS_OUTDIR/armeabi-v7a
+cp $ACBR_HOME/DLLs/Android/LibXML2/libxml2-2.9.10/aarch64-linux-android/Dynamic/*.so $JNILIBS_OUTDIR/arm64-v8a 
+cp $ACBR_HOME/DLLs/Android/LibXML2/libxslt-1.1.34/arm-linux-androideabi/Dynamic/*.so $JNILIBS_OUTDIR/armeabi-v7a 
+cp $ACBR_HOME/DLLs/Android/LibXML2/libxslt-1.1.34/aarch64-linux-android/Dynamic/*.so $JNILIBS_OUTDIR/arm64-v8a
+
+
+cd $ACBR_HOME/Projetos/ACBrLib/Android/Comum
+#compilando ACBrLibBase
+./gradlew createJar
+ls -lha
+
+
+cd $ACBR_HOME/Projetos/ACBrLib/Android/NFe
+./gradlew clean build 
+ls -lha
+
+
+KEY_PASSWORD="Iv7pj2SPSOJXcMkRtW26"
+KEYSTORE_PASSWORD="$KEY_PASSWORD"
+KEYSTORE_ALIAS=acbrlibandroidsign
+KEYSTORE_PUBLIC_KEY="${KEYSTORE_ALIAS}-public.pem"
+KEYSTORE_FILE=${KEYSTORE_ALIAS}.keystore
+
+INPUT_AAR="$ACBR_HOME/Projetos/ACBrLib/Android/NFe/ACBrLibNFe/build/outputs/aar/ACBrLibNFe-release.aar" 
+SIGNED_OUTPUT_AAR="$ACBR_HOME/Assinados/Pro/ACBrLibNFe-release.aar"
+KEYSTORE_INFO="CN=Projeto ACBr Consultoria S.A Signing Key, OU=ACBrLab, O=Projeto ACBr Consultoria S.A, L=São Paulo, ST=SP, C=BR"
+KEYSTORE_PATH="$ACBR_HOME/android-keystore"
+#tempo de expiracao do certificaod 
+VALIDITY_KEY=1800
+
+
+gerarAndroidKeystore(){
+
+	if [ ! -e "$KEYSTORE_PATH" ]; then 
+
+		mkdir $KEYSTORE_PATH
+
+		cd $KEYSTORE_PATH
+
+		#gerar chave 
+
+		keytool -genkeypair -v -keystore "$KEYSTORE_FILE" -keyalg RSA -keysize 2048 -validity "$VALIDITY_KEY" -alias "$KEYSTORE_ALIAS" -dname "$KEYSTORE_INFO" -storetype  PKCS12  -storepass "$KEY_PASSWORD" -keypass "$KEYSTORE_PASSWORD"
+
+		#exportar certificado 
+
+		keytool -exportcert -rfc -alias $KEYSTORE_ALIAS -file "${KEYSTORE_PUBLIC_KEY}" -keystore $KEYSTORE_FILE -storepass $KEYSTORE_PASSWORD -v
+	fi
+}
+
+
+assinarAAR(){
+	cd $KEYSTORE_PATH
+	jarsigner -keystore $KEYSTORE_FILE -storepass $KEYSTORE_PASSWORD -keypass $KEY_PASSWORD -signedjar $SIGNED_OUTPUT_AAR -verbose $INPUT_AAR $KEYSTORE_ALIAS
+}
+
+gerarAndroidKeystore
+assinarAAR
+
+
